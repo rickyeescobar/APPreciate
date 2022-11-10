4268854f5156bf197ec46fcd972b44d17b591d5f
Ricky Escobar <rickyeescobar1999@gmail.com>
2022-11-09 19:34:41 -0600

    Added in textblob

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, flash, redirect, render_template, request, session, url
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
 
 # configure app
 app = Flask(__name__)
@@ -23,7 +25,22 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
-    return render_template("index.html")
+    if request.method == "POST":
+        
+        phrase = request.form.get("phrase")
+        blob = TextBlob(phrase, analyzer=NaiveBayesAnalyzer())
+
+        analyzed = blob.sentiment
+
+        if analyzed["classification"] == 'pos':
+            return render_template("index.html", analyzed=analyzed)
+
+        else:
+            analyzed = "APPreciate only allows positive comments"
+            return render_template("index.html", analyzed=analyzed)
+
+    else:
+        return render_template("index.html")
 
